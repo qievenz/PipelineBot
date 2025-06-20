942c0efb9d7bfb17c2823247d33e94081e3ac4c1
qievenz <ivann96@gmail.com>
2025-06-20 20:10:27 -0300

    update shell script

--- run.bat
+++ run.bat
@@ -3,9 +3,9 @@ cd C:\git\PipelineBot
 if not exist env\Scripts\activate.bat (
     python -m venv env
     call env\Scripts\activate.bat
-    pip install -r requirements.txt
 ) else (
     call env\Scripts\activate.bat
 )
 
-python pipeline_bot.py -c 10
+pip install -r requirements.txt
+python pipeline_bot.py
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-source env/bin/activate && python3 pipeline_bot.py -c 10 #> /dev/null 2>&1
+source env/bin/activate && pip install -r requirements.txt && python3 pipeline_bot.py -c 10 #> /dev/null 2>&1
 # ./run.sh &
