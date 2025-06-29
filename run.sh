bc78bc456b55d902bb15e8583bcaed16c8a40688
qievenz <ivann96@gmail.com>
2025-06-29 16:33:01 -0300

    Updates working directory and output redirection.
    Updates the working directory path for the script.
    
    Redirects the script's standard output and standard error to /dev/null to prevent console output.

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-cd ~/PipelineBot
+cd /PipelineBot
 if [ ! -f "env/bin/activate" ]; then
     python3 -m venv env
 fi
 source env/bin/activate
 pip install -r requirements.txt
-python3 pipeline_bot.py #> /dev/null 2>&1
+python3 pipeline_bot.py > /dev/null 2>&1
 # ./run.sh &
