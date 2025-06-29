877427ff7a99babf12dd5ae94ff874b73bb2aa4e
qievenz <ivann96@gmail.com>
2025-06-29 18:06:39 -0300

    Removes 'build' option from docker compose up command
    The 'build' option was unintentionally added to the
    `docker compose up` command which is unnecessary in this context.
    This commit removes the 'build' option to streamline the
    deployment process.

--- src/docker_manager.py
+++ src/docker_manager.py
@@ -50,7 +50,7 @@ def execute_docker_compose_with_file(docker_compose_file, project_name):
         logging.info(f"Deteniendo servicios del proyecto '{project_name}' antes de reiniciar.")
         subprocess.run(['docker', 'compose', '-f', docker_compose_file, '-p', project_name, 'down'], check=True, capture_output=True)
         logging.info(f"Comando 'docker compose down' ejecutado.")
-        result = subprocess.run(['docker', 'compose', '-f', docker_compose_file, '-p', project_name, 'up', '-d', 'build'], check=True, capture_output=True, text=True)
+        result = subprocess.run(['docker', 'compose', '-f', docker_compose_file, '-p', project_name, 'up', '-d'], check=True, capture_output=True, text=True)
         logging.info(f"Docker Compose output:\n{result.stdout}")
     except subprocess.CalledProcessError as e:
         logging.error(f"Error al ejecutar Docker Compose: {e}")
