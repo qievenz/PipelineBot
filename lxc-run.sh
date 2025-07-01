3b41fc67d98222bf5fb3bfa0989dc44961c50b31
qievenz <ivann96@gmail.com>
2025-07-01 19:33:51 -0300

    crontab

--- lxc-run.sh
+++ lxc-run.sh
@@ -7,4 +7,4 @@ source env/bin/activate
 pip install -r requirements.txt
 python3 pipeline_bot.py --config '/pipeline/PipelineBot.config.json' --log '/pipeline/pipeline.log' > /dev/null 2>&1
 # /PipelineBot/./lxc-run.sh &
-# @reboot /PipelineBot/lxc-run.sh
+# @reboot /PipelineBot/lxc-run.sh >> '/pipeline/pipeline.log' 2>&1
